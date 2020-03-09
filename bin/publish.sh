79a47e529e8943319c0c277ccafc4e057587918f
Christian Mayer <christian@fox21.at>
2020-03-09 22:47:42 +0100

    Also publish lib.

--- bin/publish.sh
+++ bin/publish.sh
@@ -7,5 +7,8 @@ which cargo &> /dev/null || { echo 'ERROR: cargo not found in PATH'; exit 1; }
 
 cd "${SCRIPT_BASEDIR}/.."
 
+pushd roro_lib/ &> /dev/null
+cargo publish
+
 popd &> /dev/null
 cargo publish
